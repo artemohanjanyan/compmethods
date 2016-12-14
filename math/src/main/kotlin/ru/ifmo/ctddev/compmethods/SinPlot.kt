7ff0fc8e0a0d63a3e44f650ca2bf00f181f5633f
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-12-14 23:10:10 +0300

    Add redraw button

--- math/src/main/kotlin/ru/ifmo/ctddev/compmethods/SinPlot.kt
+++ math/src/main/kotlin/ru/ifmo/ctddev/compmethods/SinPlot.kt
@@ -5,32 +5,67 @@ import de.erichseifert.gral.plots.XYPlot
 import de.erichseifert.gral.plots.colors.SingleColor
 import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D
 import de.erichseifert.gral.ui.InteractivePanel
+import java.awt.BorderLayout
 import java.awt.Color
+import javax.swing.JButton
 import javax.swing.JFrame
 
 val PLOT_FEATURES_COLOR = Color(0.0f, 0.3f, 1.0f)
 
 class SinPlot() : JFrame() {
+
+    private val defaultWidth = 6.0
+
+    private var plot: XYPlot
+    private var data: DataTable
+    private val interactivePanel: InteractivePanel
+
     init {
-        // Create data points
-        val data = DataTable(Double::class.javaObjectType, Double::class.javaObjectType)
-        var x  = -5.0
-        while (x <= 5.0) {
-            val y = 5.0 * Math.sin(x)
-            data.add(x, y)
-            x += 0.25
+        data = DataTable(Double::class.javaObjectType, Double::class.javaObjectType)
+        data.add(-defaultWidth / 2, -defaultWidth / 2)
+        data.add(defaultWidth / 2, defaultWidth / 2)
+
+        plot = XYPlot(data)
+        plot.getAxis(XYPlot.AXIS_X).isAutoscaled = false
+        plot.getAxis(XYPlot.AXIS_Y).isAutoscaled = false
+
+        setSize(800, 600)
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        contentPane.layout = BorderLayout()
+        interactivePanel = InteractivePanel(plot)
+        contentPane.add(interactivePanel, BorderLayout.CENTER)
+        val drawButton = JButton("Draw")
+        contentPane.add(drawButton, BorderLayout.PAGE_START)
+        drawButton.addActionListener {
+            redraw()
+            interactivePanel.repaint()
         }
 
-        // Create plot
-        val plot = XYPlot(data)
+        redraw()
+    }
+
+    fun redraw() {
+        plot.remove(data)
+        data.clear()
+
+        val xAxis = plot.getAxis(XYPlot.AXIS_X)
+
+        val xStepN = 200
+        val minX = xAxis.min.toDouble()
+        val maxX = xAxis.max.toDouble()
+        val step = (maxX - minX) / xStepN
+
+        var x = minX
+        while (x <= maxX) {
+            val y = Math.sin(x)
+            data.add(x, y)
+            x += step
+        }
+
+        plot.add(data)
         plot.setLineRenderers(data, DefaultLineRenderer2D())
         plot.getPointRenderers(data)[0].color = SingleColor(PLOT_FEATURES_COLOR)
         plot.getLineRenderers(data)[0].color = PLOT_FEATURES_COLOR
-
-        // Init JFrame
-        setSize(800, 600)
-        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        contentPane.add(InteractivePanel(plot))
     }
 }
 
