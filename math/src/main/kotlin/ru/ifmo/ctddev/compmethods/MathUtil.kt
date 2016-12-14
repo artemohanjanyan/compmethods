94181fb9a563298fd83a9399e3202d836b4a7593
qradimir <rad.sorokin@gmail.com>
2016-12-14 20:24:24 +0300

    Complex calculus added

---
+++ math/src/main/kotlin/ru/ifmo/ctddev/compmethods/MathUtil.kt
@@ -0,0 +1,31 @@
+package ru.ifmo.ctddev.compmethods
+
+data class Complex(val real: Double, val imaginary: Double) {
+
+    operator fun plus(c: Complex) =
+            Complex(real + c.real, imaginary + c.imaginary)
+
+    operator fun minus(c: Complex) =
+            Complex(real - c.real, imaginary - c.imaginary)
+
+    operator fun times(d : Double) =
+            Complex(real * d, imaginary * d)
+
+    operator fun times(c: Complex) =
+            Complex(real * c.real - imaginary * c.imaginary, real * c.imaginary + imaginary * c.real)
+
+    operator fun div(d : Double) =
+            Complex(real / d, imaginary / d)
+
+    operator fun div(c : Complex) =
+            this * c.conjugate / (c.real.square() + c.imaginary.square())
+
+    val conjugate : Complex
+        get() = Complex(real, -imaginary)
+}
+
+fun Double.square() = this * this
+
+fun Double.toComplex() = Complex(this, 0.0)
+
+fun Double.toComplexImaginary() = Complex(0.0, this)
